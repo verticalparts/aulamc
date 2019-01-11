e4b69fbb12cd2303aadce65282a99de4b0843d6b
Lucas Ananias <lucas@verticalparts.com.br>
2019-01-11 08:28:12 -0200

    Projeto Criado

---
+++ src/main/java/com/verticalparts/AulamcApplication.java
@@ -0,0 +1,14 @@
+package com.verticalparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulamcApplication.class, args);
+	}
+
+}
+
