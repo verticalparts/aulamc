008a07c0cd8ee6b2ea2b92273fb9d0ce592ebd39
Lucas Ananias <lucas@verticalparts.com.br>
2019-01-11 11:55:07 -0200

    Criando operação de instanciação

--- src/main/java/com/verticalparts/AulamcApplication.java
+++ src/main/java/com/verticalparts/AulamcApplication.java
@@ -1,13 +1,32 @@
 package com.verticalparts;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.verticalparts.domain.Categoria;
+import com.verticalparts.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class AulamcApplication {
+public class AulamcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulamcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Eletrônicos");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
